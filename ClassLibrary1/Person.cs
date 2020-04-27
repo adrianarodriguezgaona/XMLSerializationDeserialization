2918c9c0202fc7bd3453a0b7f4482f4e9f44f230
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2020-04-27 11:45:46 +0200

    Class Library add with 2 classes Person and DataSerializer with 2 methods to serialize and deserialize a Person object in a XML file

---
+++ ClassLibrary1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+
+        public bool Status { get; set; }
+
+        public Person()
+        {
+
+        }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+
+        }
+
+        public Person(string firstName, string lastName,bool status):this (firstName ,  lastName)
+        {
+            Status = status;
+        }
+    }
+
+   
+}
