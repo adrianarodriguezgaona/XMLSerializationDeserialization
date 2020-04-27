8fb6024bdc63da307e17822d5690f513d543b49f
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2020-04-27 11:56:16 +0200

    Serializer to serialize a Person object into a XML file save in D:\Klanten

---
+++ XMLSerialize/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Adriana", "Maria", true);
+            string filePath = @"D:\Klanten\MyTest";
+            DataSerializer dataSerializer = new DataSerializer();
+           
+            dataSerializer.XmlSerialize(typeof(Person), person, filePath);
+            
+
+            //Console.WriteLine(filePath);
+
+
+        }
+
+
+    }
+}
