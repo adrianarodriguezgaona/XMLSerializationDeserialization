453b0549c1b45e7d21e924fad2cff96ff0d4e430
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2020-04-27 11:54:43 +0200

    Deserialize of XML file to object Person

---
+++ XmlDeserialization/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlDeserialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string filePath = @"D:\Klanten\MyTest";
+            DataSerializer dataSerializer = new DataSerializer();
+            Person person = null;
+
+            person = dataSerializer.XmlDeserialize(typeof(Person), filePath) as Person;
+
+            Console.WriteLine(person.FirstName);
+            Console.WriteLine(person.LastName);
+
+        }
+
+
+    }
+}
